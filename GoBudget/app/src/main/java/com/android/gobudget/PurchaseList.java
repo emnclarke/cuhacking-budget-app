e7e56311f409afb3683563b3ff3e6db4f3d2f786
Steve W <stevewicklund@gmail.com>
2019-02-16 16:16:56 -0500

    completed bare bones PurchaseList

--- GoBudget/app/src/main/java/com/android/gobudget/PurchaseList.java
+++ GoBudget/app/src/main/java/com/android/gobudget/PurchaseList.java
@@ -3,6 +3,7 @@ package com.android.gobudget;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -27,13 +28,46 @@ public class PurchaseList {
      * @param week - The first monday of the selected week.
      * @return - The total purchases amount for the week
      */
-    public float weeklyTotal(LocalDate week) {
-        return 0;
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public double weeklyTotal(LocalDate week) {
+        if (week.getDayOfWeek() == DayOfWeek.MONDAY) {
+            LocalDate start = week;
+            LocalDate end = week.plusDays(7);
+            return (totals(start, end));
+        } else {
+            return -1;
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
+    public double biweeklyTotal(LocalDate week) {
+        if (week.getDayOfWeek() == DayOfWeek.MONDAY) {
+            LocalDate start = week;
+            LocalDate end = week.plusDays(14);
+            return (totals(start, end));
+        } else {
+            return -1;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public double monthlyTotal(LocalDate month) {
+        LocalDate start = month.withDayOfMonth(1);
+        LocalDate end = month.withDayOfMonth(month.lengthOfMonth());
+        return (totals(start, end));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public double yearlyTotal(LocalDate year) {
+        LocalDate start = year.withDayOfYear(1);
+        LocalDate end = year.withDayOfYear(year.lengthOfYear());
+        return (totals(start, end));
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public double totals(LocalDate start, LocalDate end) {
-        float total = 0;
+        double total = 0;
         for (Purchase purchase : purchases) {
             if ((purchase.getLocalDate()).isAfter(start) && purchase.getLocalDate().isBefore(end)) {
                 total += purchase.getAmount();
