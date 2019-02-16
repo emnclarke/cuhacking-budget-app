acca422b0cce781a60ec0cc5c131db3b39d627f9
Steve W <stevewicklund@gmail.com>
2019-02-16 14:55:15 -0500

    adding purchase object

---
+++ GoBudget/app/src/main/java/com/andriod/gobudget/Purchase.java
@@ -0,0 +1,46 @@
+package com.andriod.gobudget;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
+import java.time.DayOfWeek;
+
+
+public class Purchase {
+    private float amount;
+    private LocalDateTime date;
+    private String name;
+    private String category;
+    private String description;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Purchase(float amount, String name, String category, String description) {
+        date = LocalDateTime.now();
+        this.amount = amount;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void changeCategory(String category) {
+        this.category = category;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public boolean thisWeek() {
+        LocalDate currDate = LocalDate.now();
+        LocalDate previousMonday = currDate.with(TemporalAdjusters.previous(DayOfWeek.MONDAY));
+
+        return false;
+    }
+
+}
