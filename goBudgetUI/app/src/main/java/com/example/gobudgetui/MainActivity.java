d1a29edac575f44668da845d1fb7796e575376ff
tomsloan1 <tsloan81@gmail.com>
2019-02-16 15:16:50 -0500

    vhjfyj

--- goBudgetUI/app/src/main/java/com/example/gobudgetui/MainActivity.java
+++ goBudgetUI/app/src/main/java/com/example/gobudgetui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.android.gobudget;
+package com.example.gobudgetui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,8 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.android.gobudget.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
