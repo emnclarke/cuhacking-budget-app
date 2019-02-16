f36f2ccaf47e0488eb9bffe1c5bdc5a8db2c5b99
Emily Clarke <emnclarke@gmail.com>
2019-02-16 15:43:29 -0500

    name

--- GoBudget/app/src/main/java/com/android/gobudget/MainActivity.java
+++ GoBudget/app/src/main/java/com/android/gobudget/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.gobudget.R;
+import com.android.gobudget.R;
 
 public class MainActivity extends AppCompatActivity {
 
