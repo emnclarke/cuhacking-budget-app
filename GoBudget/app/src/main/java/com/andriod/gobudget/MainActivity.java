60d61e21035f2af6e051cbcb51ce85c2bde7bf33
Emily Clarke <emnclarke@gmail.com>
2019-02-16 13:41:35 -0500

    fixing names

--- GoBudget/app/src/main/java/com/andriod/gobudget/MainActivity.java
+++ GoBudget/app/src/main/java/com/andriod/gobudget/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.gobudget;
+package com.andriod.gobudget;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.gobudget.R;
 
 public class MainActivity extends AppCompatActivity {
 
