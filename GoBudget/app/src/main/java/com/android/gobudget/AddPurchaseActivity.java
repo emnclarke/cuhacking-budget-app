3894f0d8d4a6d72765f7e809ec7611dde1fbb1db
Emily Clarke <emnclarke@gmail.com>
2019-02-17 01:21:41 -0500

    vhgvjhbhk

--- GoBudget/app/src/main/java/com/android/gobudget/AddPurchaseActivity.java
+++ GoBudget/app/src/main/java/com/android/gobudget/AddPurchaseActivity.java
@@ -38,37 +38,6 @@ public class AddPurchaseActivity extends AppCompatActivity {
         pcamount = findViewById(R.id.pcamount);
         pccategory = findViewById(R.id.pccategory);
         pcdescription = findViewById(R.id.pcdescription);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void loadPurchases(View view) {
